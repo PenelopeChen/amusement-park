0dc97d50e27d8267918d632cac0e9d76859e1662
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-02-20 18:32:57 +1100

    login, signup, logout added to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
 import { auth } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 
 export default function Home(){
     const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate();
+    // const auth = getAuth();
 
-  
+    const _logOut = ()=>{
+        signOut(auth).then(() => {
+            // Sign-out successful.
+          }).catch((error) => {
+            // An error happened.
+          });
+    }
 
+    if(!user){
+        return <div>
+            <button onClick={()=>{
+                navigate("/login")
+            }}>Log In</button>
+            <button onClick={()=>{
+                navigate("/signup")
+            }}>Sign Up</button>
 
+        </div>
+        
+    }
     return(
         <div>
             <h1>Home</h1>
             { console.log( "user information: ", user) }
+            <button onClick={ _logOut }> Log Out</button>
             
         </div>
     )
